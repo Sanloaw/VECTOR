9f02e894dd1a7e55b2a33ee4c18e0a77def7d4a4
Khadher <132037908+khadherinc@users.noreply.github.com>
2023-08-14 21:59:19 +0530

    VECTOR

---
+++ config.js
@@ -0,0 +1,81 @@
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import fetch from 'node-fetch'
+import axios from 'axios'
+
+
+global.owner = [
+  ['917560920774', 'ABDUL KHADHER', true],
+  [''],
+  [''] 
+] //Number of owners
+
+global.mods = ['917560920774','919947355090'] 
+global.prems = ['917560920774','919947355090','917025191792']
+global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
+global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
+global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
+global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
+global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
+global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
+global.lolkeysapi = ['BrunoSobrino']
+
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  xteam: 'https://api.xteam.xyz', 
+  dzx: 'https://api.dhamzxploit.my.id',
+  lol: 'https://api.lolhuman.xyz',
+  violetics: 'https://violetics.pw',
+  neoxr: 'https://api.neoxr.my.id',
+  zenzapis: 'https://zenzapis.xyz',
+  akuari: 'https://api.akuari.my.id',
+  akuari2: 'https://apimu.my.id',
+  nrtm: 'https://fg-nrtm.ddns.net',
+  bg: 'http://bochil.ddns.net',
+  fgmods: 'https://api-fgmods.ddns.net'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
+  'https://api.neoxr.my.id': `${keysneoxr}`,	
+  'https://violetics.pw': 'beta',
+  'https://zenzapis.xyz': `${keysxxx}`, 
+  'https://api-fgmods.ddns.net': 'fg-dylux'
+}
+
+// Sticker WM
+global.botname = '𝚅𝙴𝙲𝚃𝙾𝚁'
+global.premium = 'true'
+global.packname = '𝚅𝙴𝙲𝚃𝙾𝚁🎯𝙰𝙱𝙳𝚄𝙻 𝙺𝙷𝙰𝙳𝙷𝙴𝚁☘️' 
+global.author = '𝙰𝙱𝙳𝚄𝙻 𝙺𝙷𝙰𝙳𝙷𝙴𝚁' 
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/abd_khadher___\n' 
+global.dygp = 'https://chat.whatsapp.com/BOLb0ICN3sAJ5dloRBw5VD'
+global.fgsc = 'https://github.com/khadherinc/VECTOR' 
+global.fgyt = 'https://www.instagram.com/abd_khadher___'
+global.fgpyp = 'https://www.instagram.com/abd_khadher___'
+global.fglog = 'https://imgur.com/a/svb4KiN.jpg' 
+
+//imagen
+global.imagen1 = fs.readFileSync('./src/khadherinc.jpg')
+
+//Dont Edit Danger Zone⚠️
+global.wait = '*⌛ _Please Wait..._*'
+global.rwait = '⌛'
+global.dmoji = '🤭'
+global.done = '✅'
+global.error = '❌' 
+global.mlc = '📃'
+global.sdc = '🎶'
+global.xmoji = '🔥' 
+global.multiplier = 69 
+global.maxwarn = '3' // máxima advertencias
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
