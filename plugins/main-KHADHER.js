f2df7aa721e07d19ea1ffd954eee6929d66d817b
Khadher <132037908+khadherinc@users.noreply.github.com>
2023-08-15 11:49:19 +0530

    https://t.me/abd_khadher

---
+++ plugins/main-KHADHER.js
@@ -0,0 +1,18 @@
+let handler = async m => m.reply(`
+
+≡  *𝙑𝙀𝘾𝙏𝙊𝙍*   GROUPS
+▢ Join Public Bot Group And Support
+https://chat.whatsapp.com/HMJQZqjClNgKOitnskMpbJ
+─────────────
+▢ *Owner Telegram*
+https://t.me/abd_khadher
+
+▢ *Instagram*
+https://instagram.com/abd_khadher___
+
+`.trim())
+handler.help = ['gpabhi']
+handler.tags = ['main']
+handler.command = ['groups', 'gpabhi', 'abhigp', 'groupabhi','group'] 
+
+export default handler
