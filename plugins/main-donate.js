041a9f3250e21ea587767445255c8360f66ba8a4
Khadher <132037908+khadherinc@users.noreply.github.com>
2023-08-15 11:54:32 +0530

    𝙑𝙀𝘾𝙏𝙊𝙍

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -3,8 +3,8 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 
     let don = `
 ≡ *DONATION*
-Share ABHISHEK-SER Bot Link And Support. Git Link https://github.com/AbhishekSuresh2/ABHISHEK-SER`
-let img = 'https://replicate.delivery/pbxt/QbP6Fh3ZXwKON9SCB70ERGwwgeeSbztwKIOIzhUeXFkwnFHiA/out.png'
+Share ABHISHEK-SER Bot Link And Support. Git Link https://github.com/khadherinc/VECTOR`
+let img = 'https://pbxt.replicate.delivery/VeNXOQLbg1VKBa4Y5hrVam5kjDfYPTX0ekzTIompeezUWfiWE/out.png'
 conn.sendFile(m.chat, img, 'img.jpg', don, m)
 }
 
