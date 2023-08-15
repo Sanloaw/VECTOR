0b90ae117e108e9782e56cd25e4ccc567dfcd0b6
Khadher <132037908+khadherinc@users.noreply.github.com>
2023-08-15 11:25:10 +0530

    𝙑𝙀𝘾𝙏𝙊𝙍

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('ABHISHEK-SER', {
+say('𝙑𝙀𝘾𝙏𝙊𝙍', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
