38e4747136cf551a856477201763c6f59b0eac8f
Khadher <132037908+khadherinc@users.noreply.github.com>
2023-08-18 20:17:40 +0530

    MENU 2 COMMITTED #1

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -1,1 +1,35 @@
-COMMING SOON
+import fetch from 'node-fetch'
+
+let handler = async function (m, { conn, text, usedPrefix }) {
+  
+let m2 = `
+≡ _Use these commands without the prefix_
+
+▢ If you have more audios
+send me by wa.me/917560920774 the *audio + command* with which it will respond
+*No* _hy voices_
+
+
+┌─⊷ *AUDIOS*
+▢ Bot
+▢ Good morning
+▢ Good afternoon
+▢ Good night
+└──────────────
+┌─⊷ *VIDEO/GIF*
+▢ Nice night
+└──────────────`
+const pp = await (await fetch('https://i.ibb.co/qMG1JPY/fg.jpg')).buffer()
+    
+    conn.sendHydrated(m.chat, m2, '▢ VECTOR  ┃ ᴮᴼᵀ\n▢ Follow me in Instagram\nhttps://www.instagram.com/abd_khadher___\n', pp, 'wa.me/917560920774', 'WHATSAPP', null, null, [
+     ['⏍ Info', '/botinfo'],
+      ['⌬ Groups', '/groups']
+    ], m)
+    
+}
+
+handler.help = ['menu2']
+handler.tags = ['main']
+handler.command = ['menu2'] 
+handler.register = true
+export default handler
