f60e0c31f5b7e8c93464accfae8277f92e83c093
Khadher <132037908+khadherinc@users.noreply.github.com>
2023-08-19 13:23:47 +0530

    VECTOR

--- plugins/Wall-anime.js
+++ plugins/Wall-anime.js
@@ -3,7 +3,7 @@ let handler = async (m, {command, conn, usedPrefix}) => {
 let res = (await axios.get(`https://raw.githubusercontent.com/Guru322/api/Guru/BOT-JSON/anime-${command}.json`)).data  
 let haha = await res[Math.floor(res.length * Math.random())]   
 conn.sendFile(m.chat, haha, 'error.jpg', `_${command}_`, m)
-//conn.sendButton(m.chat, `_${command}_`.trim(), author, haha, [['🔄 NEXT 🔄', `${usedPrefix + command}`]], m)    
+conn.sendButton(m.chat, `_${command}_`.trim(), author, haha, [['🔄 NEXT 🔄', `${usedPrefix + command}`]], m)    
 }
 handler.command = handler.help = ['akira', 'akiyama', 'anna', 'asuna', 'ayuzawa', 'boruto', 'chiho', 'chitoge', 'deidara', 'erza', 'elaina', 'eba', 'emilia', 'hestia', 'hinata', 'inori', 'isuzu', 'itachi', 'itori', 'kaga', 'kagura', 'kaori', 'keneki', 'kotori', 'kurumi', 'madara', 'mikasa', 'miku', 'minato', 'naruto', 'nezuko', 'sagiri', 'sasuke', 'sakura']
 handler.tags = ['anime']
