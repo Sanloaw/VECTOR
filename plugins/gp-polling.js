e9a8f11894f69b2a6620f3823b9786a20fb5dcbc
Khadher <132037908+khadherinc@users.noreply.github.com>
2023-08-19 10:59:10 +0530

    𝙑𝙀𝘾𝙏𝙊𝙍

--- plugins/gp-polling.js
+++ plugins/gp-polling.js
@@ -1,8 +1,8 @@
 
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 	
-if (!args[0]) throw `✳️ Missing text for survey \n\n📌 Example : \n*${usedPrefix + command}* Message  |GURU|xd`
-if (!text.includes('|')) throw  `✳️ Separate surveys with *|* \n\n📌 Example : \n*${usedPrefix + command}* my survey|n  |GURU|xd|piro`
+if (!args[0]) throw `✳️ Missing text for survey \n\n📌 Example : \n*${usedPrefix + command}* Message  |option1|option2|option3`
+if (!text.includes('|')) throw  `✳️ Separate surveys with *|* \n\n📌 Example : \n*${usedPrefix + command}* VECTOR |n  |khadherinc|khadher`
 
 let name = await conn.getName(m.sender)
 let a = []
@@ -12,9 +12,9 @@ a.push([b[c]])
 			}
 			return conn.sendPoll(m.chat, `📊 *Survey requested by:* ${name}\n\n*Message:* ${text.split('|')[0]}`, a, m)
 }
-handler.help = ['poll <Guru|piro|xd>']
+handler.help = ['poll <khadherinc|VECTOR|MUTIDEVICE BOT>']
 handler.tags = ['group'] 
-handler.command = ['poll', 'encuesta', 'polling'] 
+handler.command = ['poll', 'polling'] 
 handler.group = true
 
 export default handler
